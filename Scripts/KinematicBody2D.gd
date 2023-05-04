d19208861d0ddebbe5743eeed11391975e56613b
antoyneGG <fabian.agg@hotmail.com>
2023-05-04 18:48:10 -0500

    Player movemente and animation working correctly

--- Scripts/KinematicBody2D.gd
+++ Scripts/KinematicBody2D.gd
@@ -5,10 +5,10 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 var vel = Vector2.ZERO
-const SPEED = 100
-const ACC = 50
+const SPEED = 200
+const ACC = 190
 const FRICC = 15
-const GRAV = 180
+const GRAV = 250
 const JUMP = 200
 var input_vector = Vector2.ZERO
 var salto = 0
@@ -51,17 +51,37 @@ func _process(delta):
 			var wr = Input.get_action_strength("Right")
 			input_vector.x = (wr - wl)
 			
+			   
 			if(input_vector != Vector2.ZERO):
-				animatedSprite.play("moverse")
+				if(not is_on_floor()):
+					if(vel.y > 0):
+						animatedSprite.play("caer")
+					elif(vel.y < 0):
+						animatedSprite.play("saltar")
+					else:
+						animatedSprite.play("saltarIdle")
+				else:
+					animatedSprite.play("moverse")
 				var to = input_vector * SPEED
 				vel = vel.move_toward(to, ACC * delta)
 				if(input_vector.x == 1):
-					scale.x = scale.y * 1
+					animatedSprite.flip_h = false
+					posHB.rotation_degrees = 0
 				elif(input_vector.x == -1):
-					scale.x *= scale.y * -1
+					animatedSprite.flip_h = true
+					posHB.rotation_degrees = 180
 			else:
-				animatedSprite.play("default")
-				vel = vel.move_toward(Vector2.ZERO, FRICC * delta)
+				if(not is_on_floor()):
+					if(vel.y > 0):
+						animatedSprite.play("caer")
+					elif(vel.y < 0):
+						animatedSprite.play("saltar")
+					else:
+						animatedSprite.play("saltarIdle")
+				else:
+					animatedSprite.play("default")
+				vel = vel.move_toward(Vector2.ZERO, 0)
+				vel.x = 0
 			
 			if(Input.is_action_just_pressed("Jump") and is_on_floor()):
 				vel.y -= JUMP
@@ -77,7 +97,7 @@ func attacking():
 	animatedSprite.play("pelea")
 
 func _on_AnimatedSprite_frame_changed():
-	print("muriendome")
+	print("muri")
 	if(animatedSprite.get_animation() == "pelea"):
 		if(animatedSprite.frame in [1, 2]):
 			collHB.set_disabled(false)
